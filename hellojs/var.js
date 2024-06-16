ad4e4c0fc4888a2d40c4358aec67edfd8adbaf7a
Arka Banerjee <arkabanerjee381@gmail.com>
2024-06-16 04:19:15 +0000

    naya file aagaya dostooooo

---
+++ hellojs/var.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "arka@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState ;
+
+// accountId = 2 //not allowed to change
+
+accountEmail = "hello@hello.com"
+accountPassword = "2234"
+accountCity = "papa ka ghar"
+
+/*
+dont use var in javascript try using let. let is better it can be used in everything
+bcz of issue in block scope and functional scope
+*/
+
+console.table ([accountId,accountEmail, accountPassword, accountCity, accountState ]);
